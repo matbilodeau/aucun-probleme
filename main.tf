497a149ec7ee5b7e366993c7eccf4fb31aa6f5cf
matbilodeau <matbilodeau@users.noreply.github.com>
2020-09-21 18:09:53 -0400

    yeah premier commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "google" {
+  project = "a2020-cr460"
+  credentials = "account.json"
+  region  = "us-east1"
+  zone    = "us-east1-c"
+}
+
+resource "google_compute_instance" "vault" {
+  name         = "vault"
+  machine_type = "f1-micro"
+  zone         = "us-east1-c"
+  tags         = ["public"]
+
+  boot_disk {
+    initialize_params {
+      image = "fedora-coreos-cloud/fedora-coreos-stable"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+}
